866c7c479d80a4ddfdb22418a61d45acb2c8ee24
dariaddz <dariadz@gmail.com>
2022-04-05 17:38:32 +0300

    Update Search.js

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,8 +1,43 @@
 import React from 'react';
 
 class Search extends React.Component {
+  state = {
+    query: '',
+  };
+  handleChange = e => {
+    this.setState({ query: e.currentTarget.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    if (this.state.query.trim() === '') {
+      return alert('Введите название картинки');
+    }
+    this.props.onSubmit(this.state.query);
+    this.setState({ query: '' });
+  };
+
   render() {
-    return <div>Это замена сеарчбара</div>;
+    return (
+      <header className="Searchbar">
+        <form className="SearchForm" onSubmit={this.handleSubmit}>
+          <button type="submit" className="SearchForm-button">
+            <span className="SearchForm-button-label">Search</span>
+          </button>
+
+          <input
+            onChange={this.handleChange}
+            className="SearchForm-input"
+            type="text"
+            value={this.state.query}
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
   }
 }
 
